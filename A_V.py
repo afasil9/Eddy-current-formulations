bd2758351c700688b85e9742d40a19a7335051aa
Arshad <af819@cam.ac.uk>
2025-10-13 15:15:10 +0100

    Implementation of interior nodes

--- A_V.py
+++ A_V.py
@@ -29,8 +29,9 @@ from dolfinx.fem import locate_dofs_topological
 
 
 degree = 1
+comm = MPI.COMM_WORLD
 
-with XDMFFile(MPI.COMM_WORLD, "copper_rod.xdmf", "r") as xdmf:
+with XDMFFile(comm, "copper_rod.xdmf", "r") as xdmf:
     domain = xdmf.read_mesh(ghost_mode=GhostMode.none)
     ct = xdmf.read_meshtags(domain, name="ct")
     tdim = domain.topology.dim
@@ -38,32 +39,35 @@ with XDMFFile(MPI.COMM_WORLD, "copper_rod.xdmf", "r") as xdmf:
     ft = xdmf.read_meshtags(domain, name="ft")
     material_tags = np.unique(ct.values)
     fdim = tdim - 1
-    domain.topology.create_connectivity(fdim, tdim)
+    domain.topology.create_connectivity(fdim, tdim) 
 
 boundary_tags = {
     "cube_boundary": 1,
     "upper_surface": 2,
     "side_surface": 3,
-    "bottom_surface": 4,
+    "bottom_surface": 4
 }
 
-vol_ids = {"copper": 1, "air": 2}
+vol_ids = {
+    "copper": 1,
+    "air": 2 
+    }
 
 
-ti = 0  # Start time
+ti = 0.0  # Start time
 T = 0.1  # End time
-num_steps = 100  # Number of time steps
+num_steps = 500  # Number of time steps
 d_t = (T - ti) / num_steps  # Time step size
 
-t = variable(fem.Constant(domain, d_t))
+t = variable(fem.Constant(domain, ti))
 dt = fem.Constant(domain, d_t)
 
-const = fem.functionspace(domain, ("DG", 0))  # Piecewise constant function space
+const = fem.functionspace(domain, ("DG", 0)) #Piecewise constant function space
 
 sigma = fem.Function(const)
 nu = fem.Function(const)
 
-sigma_air = fem.Constant(domain, default_scalar_type(1e-5))
+sigma_air = fem.Constant(domain, default_scalar_type(0.0))
 sigma_copper = fem.Constant(domain, default_scalar_type(5.96e4))
 nu_value = fem.Constant(domain, default_scalar_type(1e6))
 
@@ -72,7 +76,10 @@ sigma_values = {
     vol_ids["copper"]: sigma_copper,
 }
 
-nu_values = {vol_ids["air"]: nu_value, vol_ids["copper"]: nu_value}
+nu_values = {
+    vol_ids["air"]: nu_value,
+    vol_ids["copper"]: nu_value
+}
 
 interpolate_by_tags(sigma, sigma_values, ct)
 interpolate_by_tags(nu, nu_values, ct)
@@ -106,6 +113,7 @@ interior_nodes_array.x.scatter_forward()
 gdim = domain.geometry.dim
 facet_dim = gdim - 1
 
+
 frequency = 10.0
 omega = 2.0 * np.pi * frequency
 V_in = 10.0
@@ -113,7 +121,7 @@ V_in = 10.0
 
 outer_boundary_facets = ft.find(boundary_tags["cube_boundary"])
 
-boundary_tags_total = (1, 2, 3, 4)
+boundary_tags_total = (1,2,3,4)
 boundary_facets_V = np.concatenate([ft.find(tag) for tag in boundary_tags_total])
 
 
@@ -122,9 +130,7 @@ u_bc_V = fem.Function(V)
 u_bc_V.x.array[:] = 0
 bc_ex = fem.dirichletbc(u_bc_V, bdofs0)
 
-bdofs1 = fem.locate_dofs_topological(
-    V1, entity_dim=facet_dim, entities=outer_boundary_facets
-)
+bdofs1 = fem.locate_dofs_topological(V1, entity_dim=facet_dim, entities=outer_boundary_facets)
 u_bc_V1 = fem.Function(V1)
 u_bc_V1.x.array[:] = 0.0
 bc_ex1 = fem.dirichletbc(u_bc_V1, bdofs1)
@@ -133,8 +139,11 @@ upper_facets = ft.find(boundary_tags["upper_surface"])
 bdofs2 = fem.locate_dofs_topological(V1, entity_dim=facet_dim, entities=upper_facets)
 high_func = fem.Function(V1)
 
-high_expr = fem.Expression(V_in * ufl.sin(omega * t), V1.element.interpolation_points)
+high_expr = fem.Expression(
+    V_in * ufl.sin(omega * t), V1.element.interpolation_points
+)
 high_func.interpolate(high_expr)
+
 
 # high_func.x.array[:] = 10.0
 bc_ex2 = fem.dirichletbc(high_func, bdofs2)
@@ -145,7 +154,7 @@ low_func = fem.Function(V1)
 low_func.x.array[:] = 0.0
 bc_ex3 = fem.dirichletbc(low_func, bdofs3)
 
-bc = [bc_ex, bc_ex1, bc_ex2, bc_ex3]
+bc = [bc_ex, bc_ex1,bc_ex2, bc_ex3]
 
 u_n = fem.Function(V)
 u_n1 = fem.Function(V1)
@@ -157,24 +166,25 @@ v1 = ufl.TestFunction(V1)
 
 dx = Measure("dx", domain=domain, subdomain_data=ct)
 
-a00 = dt * ufl.inner(nu * curl(u), curl(v)) * dx + ufl.inner(sigma * u, v) * dx
 
-a01 = dt * ufl.inner(sigma * grad(u1), v) * dx
-a10 = ufl.inner(sigma * grad(v1), u) * dx
+a00 = dt * ufl.inner(nu * curl(u), curl(v)) * dx + ufl.inner(sigma * u, v) * dx(vol_ids["copper"])
 
-a11 = dt * ufl.inner(sigma * ufl.grad(u1), ufl.grad(v1)) * dx
+a01 = dt * ufl.inner(sigma * grad(u1), v) * dx(vol_ids["copper"])
+a10 = ufl.inner(sigma * grad(v1), u) * dx(vol_ids["copper"])
+
+a11 = dt * ufl.inner(sigma * ufl.grad(u1), ufl.grad(v1)) * dx(vol_ids["copper"])
 
 zero_vec = fem.Constant(domain, PETSc.ScalarType((0.0, 0.0, 0.0)))
-L0 = ufl.inner(zero_vec, v) * dx + ufl.inner(sigma * u_n, v) * dx
-L1 = ufl.inner(grad(v1), sigma * u_n) * dx
+L0 = ufl.inner(zero_vec, v) * dx + ufl.inner(sigma * u_n, v) * dx(vol_ids["copper"])
+L1 = ufl.inner(grad(v1), sigma * u_n) * dx(vol_ids["copper"])
 
 a = form([[a00, a01], [a10, a11]])
 
-comm = MPI.COMM_WORLD
 par_print(comm, "Assembling system matrix...")
 
 A_mat = assemble_matrix(a, bcs=bc)
 A_mat.assemble()
+par_print(comm, f"A matrix norm is {A_mat.norm()}")
 
 
 L = form([L0, L1])
@@ -203,8 +213,7 @@ offset_u = u_map.local_range[0] * V.dofmap.index_map_bs + u1_map.local_range[0]
 offset_u1 = offset_u + u_map.size_local * V.dofmap.index_map_bs
 
 is_u = PETSc.IS().createStride(
-    u_map.size_local * V.dofmap.index_map_bs, offset_u, 1, comm=domain.comm
-)
+    u_map.size_local * V.dofmap.index_map_bs, offset_u, 1, comm=domain.comm)
 
 is_u1 = PETSc.IS().createStride(u1_map.size_local, offset_u1, 1, comm=domain.comm)
 
@@ -260,7 +269,7 @@ else:
     Pi.assemble()
     ksp_u.getPC().setHYPRESetInterpolations(dim=domain.geometry.dim, ND_Pi_Full=Pi)
 
-# ksp_u.getPC().setHYPREAMSSetInteriorNodes(interior_nodes_array.x.petsc_vec)
+ksp_u.getPC().setHYPREAMSSetInteriorNodes(interior_nodes_array.x.petsc_vec)
 
 opts = PETSc.Options()
 opts[f"{ksp_u.prefix}pc_hypre_ams_cycle_type"] = 13
@@ -274,6 +283,8 @@ opts[f"{ksp_u.prefix}pc_hypre_ams_relax_type"] = 2
 opts[f"{ksp_u.prefix}pc_hypre_ams_relax_weight"] = 1.0
 opts[f"{ksp_u.prefix}pc_hypre_ams_relax_times"] = 1
 opts[f"{ksp_u.prefix}pc_hypre_ams_omega"] = 1.0
+opts[f"{ksp_u.prefix}pc_hypre_ams_projection_frequency"] = 8
+
 
 ksp_u.setFromOptions()
 
@@ -296,20 +307,23 @@ ksp_u1.getPC().setUp()
 # pc.setFactorSolverType("mumps")
 
 
-# ksp.setMonitor(my_monitor)
+ksp.setMonitor(my_monitor)
+
 
 u_n_prev = u_n.copy()
 
+par_print(comm, f"b norm is, {b.norm()}")
 sol = A_mat.createVecRight()
 
 par_print(comm, "about to solve")
 ksp.solve(b, sol)
 
+
 uh, uh1 = Function(V), Function(V1)
 offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
 
 uh.x.array[:offset] = sol.array_r[:offset]
-uh1.x.array[: (len(sol.array_r) - offset)] = sol.array_r[offset:]
+uh1.x.array[:(len(sol.array_r) - offset)] = sol.array_r[offset:]
 
 uh.x.scatter_forward()
 uh1.x.scatter_forward()
@@ -333,52 +347,63 @@ vector_vis = fem.functionspace(
 )
 
 A_vis = Function(vector_vis)
-A_file = VTXWriter(domain.comm, "A_field.bp", A_vis, "BP4")
 A_vis.interpolate(u_n)
+A_file = VTXWriter(domain.comm, "A_field_interior.bp", A_vis, "BP4")
 A_file.write(t.expression().value)
 
 B = curl(u_n)
 B_vis = Function(vector_vis)
-B_file = VTXWriter(domain.comm, "B_field.bp", B_vis, "BP4")
 Bexpr = fem.Expression(B, vector_vis.element.interpolation_points)
 B_vis.interpolate(Bexpr)
+B_file = VTXWriter(domain.comm, "B_field_interior.bp", B_vis, "BP4")
 B_file.write(t.expression().value)
 
 
 da_dt = (u_n - u_n_prev) / dt
-E = -grad(u_n1) - da_dt
+E = - grad(u_n1) - da_dt
 E_vis = Function(vector_vis)
 Eexpr = fem.Expression(E, vector_vis.element.interpolation_points)
 E_vis.interpolate(Eexpr)
-E_file = VTXWriter(domain.comm, "E_field.bp", E_vis, "BP4")
+E_file = VTXWriter(domain.comm, "E_field_interior.bp", E_vis, "BP4")
 E_file.write(t.expression().value)
 
 J_ind = sigma * E
 J_vis = Function(vector_vis)
 Jexpr = fem.Expression(J_ind, vector_vis.element.interpolation_points)
 J_vis.interpolate(Jexpr)
-J_file = VTXWriter(domain.comm, "J_field.bp", J_vis, "BP4")
+J_file = VTXWriter(domain.comm, "J_field_interior.bp", J_vis, "BP4")
 J_file.write(t.expression().value)
 
-u_n1_file = VTXWriter(domain.comm, "u_n1_field.bp", u_n1, "BP4")
+u_n1_file = VTXWriter(domain.comm, "u_n1_field_interior.bp", u_n1, "BP4")
 u_n1_file.write(t.expression().value)
 
-output_freq = 5
+par_print(comm, f"L2 norm of u_n is {L2_norm(u_n)}")
+par_print(comm, f"L2 norm of u_n1 is {L2_norm(u_n1)}")
+par_print(comm, f"L2 norm of B is {L2_norm(B)}")
+par_print(comm, f"L2 norm of J is {L2_norm(J_ind)}")
+par_print(comm, f"L2 norm of E is {L2_norm(E)}")
+
+output_freq = 20
 
 for n in range(num_steps):
+
+    par_print(comm, "\n")
+
     t.expression().value += d_t
-    par_print(comm, f"Time step {n + 1}: t = {t.expression().value}")
+    par_print(comm, f"Time step {n+1}: t = {t.expression().value}")
+    ksp_u.getPC().HYPREAMSResetSolveCounter()
 
     u_n_prev = u_n.copy()
 
     high_expr = fem.Expression(
-        V_in * ufl.sin(omega * t), V1.element.interpolation_points
+    V_in * ufl.sin(omega * t), V1.element.interpolation_points
     )
     high_func.interpolate(high_expr)
 
     bc_ex2 = fem.dirichletbc(high_func, bdofs2)
-    bc = [bc_ex, bc_ex1, bc_ex2, bc_ex3]
 
+    bc = [bc_ex, bc_ex1, bc_ex2, bc_ex3]
+    
     uh.x.array[:] = 0
     uh1.x.array[:] = 0
 
@@ -389,14 +414,16 @@ for n in range(num_steps):
     bcs0 = bcs_by_block(extract_function_spaces(L), bc)
     set_bc(b, bcs0)
 
-    par_print(comm, b.norm())
+    par_print(comm, f"b norm is, {b.norm()}")
     sol = A_mat.createVecRight()
 
     par_print(comm, "about to solve")
     ksp.solve(b, sol)
 
+    par_print(comm, f"Sol norm is, {sol.norm()}")
+
     uh.x.array[:offset] = sol.array_r[:offset]
-    uh1.x.array[: (len(sol.array_r) - offset)] = sol.array_r[offset:]
+    uh1.x.array[:(len(sol.array_r) - offset)] = sol.array_r[offset:]
 
     uh.x.scatter_forward()
     uh1.x.scatter_forward()
@@ -416,7 +443,34 @@ for n in range(num_steps):
     par_print(comm, f"L2 norm of u_n is {L2_norm(u_n)}")
     par_print(comm, f"L2 norm of u_n1 is {L2_norm(u_n1)}")
 
-    if (n + 1) % output_freq == 0:
+
+    A_vis.interpolate(u_n)
+    A_file.write(t.expression().value)
+
+    B = curl(u_n)
+    Bexpr = fem.Expression(B, vector_vis.element.interpolation_points)
+    B_vis.interpolate(Bexpr)
+    B_file.write(t.expression().value)
+
+    da_dt = (u_n - u_n_prev) / dt
+    E = -grad(u_n1) - da_dt
+    Eexpr = fem.Expression(E, vector_vis.element.interpolation_points)
+    E_vis.interpolate(Eexpr)
+    E_file.write(t.expression().value)
+
+    J_ind = sigma * E
+    Jexpr = fem.Expression(J_ind, vector_vis.element.interpolation_points)
+    J_vis.interpolate(Jexpr)
+    J_file.write(t.expression().value)
+
+    u_n1_file.write(t.expression().value)
+
+    par_print(comm, f"L2 norm of B is {L2_norm(B)}")
+    par_print(comm, f"L2 norm of J is {L2_norm(J_ind)}")
+    par_print(comm, f"L2 norm of E is {L2_norm(E)}")
+
+
+    if (n + 1) % output_freq == 0:   
         A_vis.interpolate(u_n)
         A_file.write(t.expression().value)
 
@@ -437,6 +491,10 @@ for n in range(num_steps):
         J_file.write(t.expression().value)
 
         u_n1_file.write(t.expression().value)
+
+        par_print(comm, f"L2 norm of B is {L2_norm(B)}")
+        par_print(comm, f"L2 norm of J is {L2_norm(J_ind)}")
+        par_print(comm, f"L2 norm of E is {L2_norm(E)}")
 
 A_file.close()
 B_file.close()
